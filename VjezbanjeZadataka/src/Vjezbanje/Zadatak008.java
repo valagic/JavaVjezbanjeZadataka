85fc7e18fe5b537f5d4c77b2362b70e8f1d07800
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-06-24 10:19:42 +0200

    basic

---
+++ VjezbanjeZadataka/src/Vjezbanje/Zadatak008.java
@@ -0,0 +1,24 @@
+package Vjezbanje;
+
+public class Zadatak008 {
+
+	public static void main(String[] args) {
+		
+		/*  Write a Java program to display the following pattern. Go to the editor
+		Sample Pattern :
+		
+		   J    a   v     v  a                                                  
+		   J   a a   v   v  a a                                                 
+		J  J  aaaaa   V V  aaaaa                                                
+		 JJ  a     a   V  a     a      */
+		
+		System.out.println("   J    a   v     v  a     ");
+		System.out.println("   J   a a   v   v  a a     ");
+		System.out.println("J  J  aaaaa   V V  aaaaa    ");
+		System.out.println(" JJ  a     a   V  a     a");
+		
+		
+		
+		
+	}
+}
