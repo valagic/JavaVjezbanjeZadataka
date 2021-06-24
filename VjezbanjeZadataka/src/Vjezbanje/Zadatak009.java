39cd460e86d1fab6f2bb4abc996f2e3771eb42d4
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-06-24 10:21:05 +0200

    basic

---
+++ VjezbanjeZadataka/src/Vjezbanje/Zadatak009.java
@@ -0,0 +1,20 @@
+package Vjezbanje;
+
+public class Zadatak009 {
+
+	public static void main(String[] args) {
+		
+		 /* Write a Java program to compute the specified expressions and print the output. Go to the editor
+		 Test Data:
+		 ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+		 Expected Output
+		 2.138888888888889 */
+		
+		System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+		
+		
+		
+		
+		
+	}
+}
