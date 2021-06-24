ce69b39ff7e0de7077b78675d2d21bdae8ef5b23
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-06-24 14:58:32 +0200

    for

---
+++ VjezbanjeZadataka/src/Vjezbanje/Zadatak017.java
@@ -0,0 +1,15 @@
+package Vjezbanje;
+
+public class Zadatak017 {
+
+	public static void main(String[] args) {
+		
+		// Write a program in Java to display the first 10 natural numbers.
+		
+		int i;
+		System.out.println("prvih 10 brojeva su : \n");
+		for(i=1;i<=10;i++) {
+			System.out.println(i);
+		}
+	}
+}
