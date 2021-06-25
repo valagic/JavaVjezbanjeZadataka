f27021aa7301d2ead07a0914d1d6c8993fa9cb10
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-06-25 22:47:52 +0200

    dodavanje zadatka

--- VjezbanjeZadataka/src/Vjezbanje/Zadatak025.java
+++ VjezbanjeZadataka/src/Vjezbanje/Zadatak025.java
@@ -6,6 +6,24 @@ public class Zadatak025 {
 
 	public static void main(String[] args) {
 		
+		/* Write a program in Java to display the pattern like right angle triangle with a number. Go to the editor
+			
+			Test Data
+			Input number of rows : 10
+			Expected Output :
+			
+			1                                                                                
+			12                                                                               
+			123                                                                              
+			1234                                                                             
+			12345                                                                            
+			123456                                                                           
+			1234567                                                                          
+			12345678                                                                         
+			123456789                                                                        
+			12345678910 */
+		
+		
 		int n,i,j;
 		
 		Scanner ulaz = new Scanner(System.in);
