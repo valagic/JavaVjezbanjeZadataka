d4f14f7cee37afff4f951a36ec5b679fbdb8aa3e
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-06-28 11:56:23 +0200

    String - compareToIgnoreCase - ignorira velika i mala slova

--- VjezbanjeZadataka/src/Vjezbanje/Zadatak043.java
+++ VjezbanjeZadataka/src/Vjezbanje/Zadatak043.java
@@ -1,24 +1,24 @@
 package Vjezbanje;
 
-public class Zadatak042 {
+public class Zadatak043 {
 
 	public static void main(String[] args) {
 		
-		/* Write a Java program to compare two strings lexicographically. Two strings are lexicographically equal if they are the same length and contain the same characters in the same positions. Go to the editor
-
-		Sample Output:
-		
-		String 1: This is Exercise 1                                                                                  
-		String 2: This is Exercise 2                                                                                  
-		"This is Exercise 1" is less than "This is Exercise 2" */
+		/* Write a Java program to compare two strings lexicographically, ignoring case differences. Go to the editor
+			
+			Sample Output:
+			
+			String 1: This is exercise 1                                                                                  
+			String 2: This is Exercise 1                                                                                  
+			"This is exercise 1" is equal to "This is Exercise 1" */
 		
 		String jedan = "Ovo je vjezba 1";
-		String dva = "Ovo je vjezba 2";
+		String dva = "Ovo je Vjezba 1";
 		
 		System.out.println("String 1 : " + jedan);
 		System.out.println("String 2 : " + dva);
 		
-		int rezultat = jedan.compareTo(dva); // radi komparaciju dva stringa
+		int rezultat = jedan.compareToIgnoreCase(dva); // radi komparaciju dva stringa, ignorira velika i mala slova
 		
 		if(rezultat<0) {
 			System.out.println("\"" + jedan + "\"" +
@@ -38,7 +38,4 @@ public class Zadatak042 {
         }
 	}
 }
-
-
-
 
